4dbf12ced6396fb6c962a1cb89d4775ba09e906a
wzhangyubin@163.com <wzhangyubin@163.com>
2026-04-08 14:32:40 +0800

    删除

--- games/54785/src/settings.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default","ball"],collisionMatrix:[[true,true],[true,false]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/MainScene.fire",orientation:"",jsList:[]};
--- games/55521/src/settings.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-desktop",groupList:["default","UI"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/main/loadScene.fire",orientation:"",jsList:[]};
